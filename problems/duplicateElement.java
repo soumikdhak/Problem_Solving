33d752f4fb37a6f4a277aceb2c6180bbbfc46f7c
soumikdhak <soumikdhak445@gmail.com>
2025-12-02 01:27:11 +0530

    refactor: improve duplicate element detection and add recursion functionality

--- problems/duplicateElement.java
+++ problems/duplicateElement.java
@@ -1,21 +1,31 @@
-// 1. Find duplicate elements in an array.
-
 import java.util.Scanner;
 
 public class duplicateElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of the Array : ");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        //int newarr[]=new int[]
 
-        for(int i=0; i<size;i++){
-            System.out.print("Element ["+i+"] : ");
-            arr[i]=sc.nextInt();
+        System.out.print("Enter the size of the Array : ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Element [" + i + "] : ");
+            arr[i] = sc.nextInt();
         }
 
-        //for()
+        System.out.print("Duplicate Elements : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    System.out.print(arr[i] + " ");
+                    break;   // prevents printing same duplicate multiple times
+                }
+            }
+        }
+
         sc.close();
     }
 }
--- recursion/recursion4.java
+++ recursion/recursion4.java
@@ -6,15 +6,28 @@ import java.util.Scanner;
 
 public class recursion4 {
 
-    public static void printSquare(){
+    public static void printSquare(int x, int n, int value){
+        if(n==0) {
+            System.out.println("The Value of X*N is : "+value);
+            return;
+        }
+
+        value*=x;
+
+        printSquare(x, n-1, value);
 
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the value of X : ");
         int x = sc.nextInt();
+        System.out.print("Enter the value of N : ");
         int n = sc.nextInt();
 
-        
+        int value=1;
+
+        printSquare(x,n,value);
+        sc.close();
     }
 }
